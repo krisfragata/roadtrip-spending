bc2b29295009628860bd505ed0afb8a7722d0298
Kris Fragata <kristinnefragata@outlook.com>
2023-11-10 10:54:34 -0800

    adding more design comments

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
+$(document).ready(()=>{
+
+    window.addEventListener("load", function(event) {
+
 //input: num
 //output: total
 //declare a function that asks users for input
-function addInput(dollar){
-//declare a var to hold sum
+function addInput(){
+    //declare an obj to hold data
+    //declare a var to hold sum
+    //return a func that takes in one input, dollar
+        //get today's date
+        //check to see if today's date is already in obj
+        //if yes, create a new key with dollar amount in arr as val
+        //update sum
+        //return sum
+
 
 }
 
 
-//declare a function that displays data
+//declare a function that calculates day
+function calculateDays(){
+    //declare a var to hold curr day
+    //calculate days since beginning of trip
+    //return days since beginning trip
+}
 
 //declare a function that displays average spending
+function calculateAvg(sum, days){
+    //return the average of sum in days
+}
+
+const roadtrip23 = addInput();
+const days = calculateDays();
+const currSum = roadtrip23();
+const avgSpent = calculateAvg(currSum, days);
+
+
+    });
+});
