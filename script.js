5e836284514934f5395c271ce252a86dd295713e
Kris Fragata <79350471+krisfragata@users.noreply.github.com>
2023-11-10 11:31:42 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+//input: num
+//output: total
+//declare a function that asks users for input
+function addInput(dollar){
+
+}
+
+
+//declare a function that displays data
+
+//declare a function that displays average spending
